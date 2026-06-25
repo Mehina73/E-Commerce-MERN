090cc6afa55b3d66b831e1ad252b31fd573d3761
Mehina73 <mohamedmehina32@gmail.com>
2026-06-25 21:09:14 +0300

    Add item to cart

--- frontend/src/context/Cart/CartProvider.tsx
+++ frontend/src/context/Cart/CartProvider.tsx
@@ -1,15 +1,57 @@
 import { useState, type FC, type PropsWithChildren } from "react";
 import { CartContext } from "./CartContext";
 import type { CartItem } from "../../types/CartItem";
+import { useAuth } from "../Auth/AuthContext";
 
 
 const CartProvider: FC<PropsWithChildren> = ({ children }) => {
-   
+    const { token } = useAuth();
     const [cartItem, setCartItem] = useState<CartItem[]>([]);
     const [totalAmount, setTotalAmount] = useState<number>(0);
-    
-    const addItemToCart = (productId: string) =>{
-        console.log(productId);
+    const [error, setError] = useState('');
+
+    const addItemToCart = async (productId: string) => {
+        try {
+            const response = await fetch('http://localhost:3001/cart/item', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify({ productId, quantity: 1 })
+            })
+
+            if (!response.ok) {
+                setError('Failed to add to cart');
+            }
+
+            const cart = await response.json();
+
+            if (!cart) {
+                setError('Failed to parse cart data')
+            }
+
+
+            const cartItemsMapped = cart.items.map(
+                ({ product, quantity }: any) => ({
+                    productId: product._id,
+                    title: product.title,
+                    image: product.image,
+                    quantity,
+                    unitPrice: product.unitPrice,
+                })
+            );
+
+            setCartItem(cartItemsMapped);
+            setTotalAmount(cart.totalAmount)
+
+        } catch (error) {
+            console.error(error);
+        }
+
+
+
+        
     }
 
 
--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,45 +1,47 @@
 
-import { useAuth }  from "../context/Auth/AuthContext";
-import { Container, Typography } from "@mui/material";
+import { useAuth } from "../context/Auth/AuthContext";
+import { Box, Container, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
+import { useCart } from "../context/Cart/CartContext";
 
 
 const CartPage = () => {
     const { token } = useAuth();
-    const [cart, setCart] = useState();
+    const { cartItem, totalAmount } = useCart();
     const [error, setError] = useState("");
 
-    useEffect(() => {
-        if (!token) {
-            return;
-        }
+    // useEffect(() => {
+    //     if (!token) {
+    //         return;
+    //     }
 
 
-        const fetchCart = async () => {
-            const response = await fetch("http://localhost:3001/mycart", {
-                headers: { Authorization: `Bearer ${token}` }
-            })
+    //     const fetchCart = async () => {
+    //         const response = await fetch("http://localhost:3001/mycart", {
+    //             headers: { Authorization: `Bearer ${token}` }
+    //         })
 
 
-            if (!response.ok) {
-                setError("Failed to fetch user cart. Please try again");
-                return;
-            }
+    //         if (!response.ok) {
+    //             setError("Failed to fetch user cart. Please try again");
+    //             return;
+    //         }
 
-            const data = await response.json();
-            setCart(data);
+    //         const data = await response.json();
+    //         setCart(data);
 
-        };
+    //     };
 
-        fetchCart();
-    }, [token]);
+    //     fetchCart();
+    // }, [token]);
 
-    console.log({ cart });
-
-
+    console.log(cartItem);
     return (
         <Container sx={{ mt: 2 }}>
             <Typography variant="h4">My Cart</Typography>
+            {cartItem.map((item) => (
+                <Box> {item.title}</Box>
+            ))}
         </Container>
     )
 
